e104ffc9c1d6e9a79bd4d159ac54ebe76ba56848
Danil <danil.shevelev.2004@mail.ru>
2024-11-04 00:20:24 +0500

    js files deleted

--- js/htmlAttrs.js
+++
@@ -1,6 +0,0 @@
-export function makeHtmlAttrNameSelector(name) {
-    return "[" + name + "]";
-}
-export function makeHtmlAttrNameValueSelector(name, value) {
-    return "[" + name + "=" + "\"" + value + "\"" + "]";
-}
--- js/noOverwrite.js
+++
@@ -1,8 +0,0 @@
-export function noOverwriteFilesInInput(inputEl, storage) {
-    inputEl.addEventListener("input", () => {
-        Array.from(inputEl.files).forEach((file) => {
-            storage.addFile(file);
-        });
-        storage.updateInput();
-    });
-}
